26c81d18c60ae794a8f7f950483d5e5cea2f0d39
YongJoon Joe <developer@quietjoon.net>
2020-10-18 00:10:40 +0900

    Fix: Printing min element twice

--- src/Data/ArchCEREScript/Data/Show.hs
+++ src/Data/ArchCEREScript/Data/Show.hs
@@ -8,35 +8,22 @@ import Data.List
 
 import TextShow ()
 import TextShow as TS
-    ( TextShow(showb), fromLazyText, fromString, singleton, Builder )
 
 
 instance TextShow a => TextShow (IntMap a) where
   showb im = TS.singleton '[' <> mapInternal <> TS.singleton ']'
    where
     mapInternal :: Builder
-    mapInternal =
-      if SIM.null im
-        then nothing -- how to return nothing
-        else SIM.foldrWithKey (\k v b -> b <> TS.singleton ',' <> renderKV k v) base im
-     where
-      nothing = fromString ""
-      (hk, hv) = fromJust . SIM.lookupMin $ im
-      renderKV k v = TS.singleton '(' <> showb k <> TS.singleton '|' <> showb v <> TS.singleton ')'
-      base = renderKV hk hv
+    mapInternal = foldr1 (<>) . intersperse (fromLazyText ",") $ builderList
+    builderList = Prelude.map renderKV . SIM.toList $ im
+    renderKV (k, v) = TS.singleton '(' <> showb k <> TS.singleton '|' <> showb v <> TS.singleton ')'
 instance (TextShow idx, TextShow a) => TextShow (Map idx a) where
-  showb im = TS.singleton '[' <> mapInternal <> TS.singleton ']'
+  showb m = TS.singleton '[' <> mapInternal <> TS.singleton ']'
    where
     mapInternal :: Builder
-    mapInternal =
-      if SM.null im
-        then nothing -- how to return nothing
-        else SM.foldrWithKey (\k v b -> b <> TS.singleton ',' <> renderKV k v) base im
-     where
-      nothing = fromString ""
-      (hk, hv) = fromJust . SM.lookupMin $ im
-      renderKV k v = TS.singleton '(' <> showb k <> TS.singleton '|' <> showb v <> TS.singleton ')'
-      base = renderKV hk hv
+    mapInternal = foldr1 (<>) . intersperse (fromLazyText ",") $ builderList
+    builderList = Prelude.map renderKV . SM.toList $ m
+    renderKV (k, v) = TS.singleton '(' <> showb k <> TS.singleton '|' <> showb v <> TS.singleton ')'
 
 showbList :: TextShow a => [a] -> Builder
 showbList aList = TS.singleton '[' <> mapInternal <> TS.singleton ']'
