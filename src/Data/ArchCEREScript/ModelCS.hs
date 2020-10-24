46b18122195ab7369f1fad71a1bedf0c8911b2ae
YongJoon Joe <developer@quietjoon.net>
2020-10-24 11:48:46 +0900

    Move Value from Data.ArchCEREScript.Model.ValueContainer to Data.ArchCEREScript.Model.Value

--- src/Data/ArchCEREScript/ModelCS.hs
+++ src/Data/ArchCEREScript/ModelCS.hs
@@ -5,7 +5,7 @@ import Data.ArchCEREScript.VariablePosition
 --import Data.ArchCEREScript.Model.Operator
 import Data.ArchCEREScript.Model.Operator
 import Data.ArchCEREScript.Model.ValueContainer
-import Data.ArchCEREScript.Model.ValueType
+import Data.ArchCEREScript.Model.Value
 import Data.ArchCEREScript.Model.VariableIndex
 import Data.ArchCEREScript.Model.VariablePlace
 
--- src/Parser/ArchCEREScript/Model/Value.hs
+++ src/Parser/ArchCEREScript/Model/Value.hs
@@ -1,10 +1,10 @@
-module Parser.ArchCEREScript.Model.ValueType where
+module Parser.ArchCEREScript.Model.Value where
 
 
 import Text.Megaparsec
 import Text.Megaparsec.Char
 
-import Data.ArchCEREScript.Model.ValueType
+import Data.ArchCEREScript.Model.Value
 import Parser.ArchCEREScript.Type
 
 
@@ -13,13 +13,16 @@ import Parser.ArchCEREScript.Type
 parseValueType :: Parser ValueType
 parseValueType = choice
   [ VTInt <$ string "VT-Int"
-  , VTDbl <$ string "VT-Dbl"
-  , VTStr <$ string "VT-Str"
+  , VTFlt <$ string "VT-Dbl"
+  , VTText <$ string "VTText"
   , VTBool <$ string "VTBool"
   , VTAtom <$ string "VTAtom"
   , VTArr <$ string "VT-Arr"
+  , VTIMap <$ string "VTIMap"
+  , VTNMap <$ string "VTNMap"
   , VTPtr <$ string "VT-Ptr"
   , VTScr <$ string "VT-Scr"
   , VTRct <$ string "VT-Rct"
+  , VTRStr <$ string "VTRStr"
   , VTErr <$ string "VT-Err"
   ]
