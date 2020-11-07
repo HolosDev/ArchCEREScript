91c0d69482788e34fe89a2c0b36ff102268cb24a
YongJoon Joe <developer@quietjoon.net>
2020-11-07 16:55:59 +0900

    Fix: Resolve VariablePosition sharing between MEIS and MCS
    In strictly, this is not bug.
    However, for showing compatibility between EIS and ArchCERES, the definition should not introduce a new EIS for MEIS because the type of VariablePosition between EIS and ArchCERES would be different.

--- src/Data/ArchCEREScript/Model/EIS.hs
+++ src/Data/ArchCEREScript/Model/EIS.hs
@@ -5,6 +5,7 @@ module Data.ArchCEREScript.Model.EIS where
 
 import TextShow
 
+import Data.ArchCEREScript
 import Data.ArchCEREScript.Model.Operator
 import Data.ArchCEREScript.Model.Value
 import Data.ArchCEREScript.Model.ValueContainer
@@ -13,19 +14,24 @@ import Data.ArchCEREScript.Model.VariablePlace
 import Data.ArchCEREScript.Model.VariablePosition
 
 
-data NEIS
+type ModelCS = ArchCEREScript MEIS VariablePosition VariableIndex ValueContainer Value VariablePlace ValueType CERESOperator
+type MCS = ArchCERES MEIS VariablePosition VariableIndex ValueContainer Value VariablePlace ValueType CERESOperator
+type MVPosition = VariablePosition MEIS VariableIndex ValueContainer Value VariablePlace ValueType CERESOperator
+type MVIndex = VariableIndex MEIS ValueContainer Value VariablePlace ValueType CERESOperator
+type MVContainer = ValueContainer MEIS Value VariablePlace CERESOperator
+type MV = Value MEIS VariablePlace CERESOperator
 
-type NMVariablePosition = VariablePosition NEIS VariableIndex ValueContainer Value VariablePlace ValueType CERESOperator
-type NMVariableIndex = VariableIndex NEIS ValueContainer Value VariablePlace ValueType CERESOperator
-type NMValueContainer = ValueContainer NEIS Value VariablePlace CERESOperator
-type NMValue = Value NEIS VariablePlace CERESOperator
 
-data family MEIS vP vi vc v vp vt co
-data instance MEIS NMVariablePosition NMVariableIndex NMValueContainer NMValue VariablePlace ValueType CERESOperator = MEIS0
-type MEIS0 = MEIS NMVariablePosition NMVariableIndex NMValueContainer NMValue VariablePlace ValueType CERESOperator
+data MEIS
+  = MEIS0
+  | MEIS1 {rVP1 :: VariablePosition MEIS VariableIndex ValueContainer Value VariablePlace ValueType CERESOperator}
 
-instance Show (MEIS NMVariablePosition NMVariableIndex NMValueContainer NMValue VariablePlace ValueType CERESOperator) where
+getRVP1 :: ArchCERES MEIS VariablePosition VariableIndex ValueContainer Value VariablePlace ValueType CERESOperator -> VariablePosition MEIS VariableIndex ValueContainer Value VariablePlace ValueType CERESOperator
+getRVP1 (CRSExt eis) = rVP1 (eis :: MEIS)
+getRVP1 acs = rVP1 (acs :: ArchCERES MEIS VariablePosition VariableIndex ValueContainer Value VariablePlace ValueType CERESOperator)
+
+instance Show MEIS where
   show = toString . showb
 
-instance TextShow (MEIS NMVariablePosition NMVariableIndex NMValueContainer NMValue VariablePlace ValueType CERESOperator) where
+instance TextShow MEIS where
   showb MEIS0 = fromText "MEIS0"
--- src/Data/ArchCEREScript/Model/MCS.hs
+++ src/Data/ArchCEREScript/Model/MCS.hs
@@ -22,12 +22,6 @@ import Data.ArchCEREScript.Model.VariablePlace
 import Data.ArchCEREScript.Model.VariablePosition
 
 
-type ModelCS = ArchCEREScript MEIS0 VariablePosition VariableIndex ValueContainer Value VariablePlace ValueType CERESOperator
-type MCS = ArchCERES MEIS0 VariablePosition VariableIndex ValueContainer Value VariablePlace ValueType CERESOperator
-type MVPosition = VariablePosition MEIS0 VariableIndex ValueContainer Value VariablePlace ValueType CERESOperator
-type MVIndex = VariableIndex MEIS0 ValueContainer Value VariablePlace ValueType CERESOperator
-type MVContainer = ValueContainer MEIS0 Value VariablePlace CERESOperator
-type MV = Value MEIS0 VariablePlace CERESOperator
 type MVPlace = VariablePlace
 type MVType = ValueType
 type MCSOp = CERESOperator
